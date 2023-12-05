f3e4630003dd27f28552096a89700370a77bd0da
SarahEBruce <bruce.sarahemily@gmail.com>
2023-12-05 16:34:27 +0000

    Create 2 choice variables and playRound function
    playerSelection and computerSelection variables are declared and compared within the function to reveal a winner of each round, or a tie.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // The computer generates a choice at random and returns the winner
 
 //Store all 3 items in a single variable to allow random selection
-const gameChoices = ["Rock", "Paper", "Scissors"]
+const gameChoices = ["rock", "paper", "scissors"];
 
 // Function generates a random integer between 0 and 2
 function getComputerChoice() {
@@ -15,3 +15,43 @@ function getComputerChoice() {
     //randomIndex determines array position of gameChoices returned
     return gameChoices [randomIndex];
 }
+
+//Store function in computerSelection variable
+//Store user's choice in playerSe;ection variable
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+
+//Function takes the playerSelection and computerSelection variable and compares them
+//Variables are compared in lower case to ensure equality for comparison
+//A draw results in replay, otherwise the user is informed whether they won or lost
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "It's a draw - play again!";
+    }
+        
+        else if ((playerSelection.toLowerCase() === "rock") && (computerSelection === "paper")) {
+        return "You lose! Paper beats Rock!";
+        }
+
+        else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "scissors")) {
+        return "You win! Rock beats Paper!";
+        }
+
+        else if ((playerSelection.toLowerCase() === "paper" && computerSelection === "rock")) {
+            return "You win! Paper beats Rock!";
+        }
+                
+        else if ((playerSelection.toLowerCase() === "paper" && computerSelection === "scissors")) {
+            return "You lose! Scissors beats Paper!";
+        }
+        
+        else if ((playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")) {
+            return "You win! Scissors beats Paper!";
+        }
+        
+        else if ((playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")) {
+            return "You win! Scissors beats Paper!";
+        }
+        
+}
+
